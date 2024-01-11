6bccb74fd7fcd37d9841a8cafb610d1d4995d24f
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-01-11 16:54:54 -0500

    fixed my navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import postData from "./data/posts.json";
+import Nav from "./Components/Nav";
 
 function App() {
   console.log(postData);
   return (
-    <main>
-      <button className="btn">hi</button>
+    <main class= "">
+      <Nav />
+      <form class= "grid gap-3" role="search">
+      Search posts by location...
+        <input class= "form-control me-2" type= "search"  ></input> 
+       
+        <button class="btn black-text-warning text-bg-warning p-3"  type="submit"> Cancel </button>
+      </form>
     </main>
   );
 }
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,17 @@
+const Nav = () => {
+   return (
+
+        <nav class="nav grid gap-3">
+            <p class="font-size-lg">Travel <span class= "script-font text-warning">Blog</span></p>
+            <a class="nav-link link-secondary" href="#">About</a>
+            <a class="nav-link link-secondary" href="#">Popular posts</a>
+            <div class= "container-fluid script-font">
+            Travel is the only thing that makes you richer...
+            </div>
+        </nav>
+       
+    )
+
+}
+
+export default Nav;
